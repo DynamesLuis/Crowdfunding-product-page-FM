0fed3afcb8c926ab60dc7891ef82e83e87c69aa8
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-10-01 14:13:17 -0600

    feat: bookmark and back project button events

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const $navBtn = document.querySelector(".nav-btn");
+const $backBtn = document.querySelector(".back-btn");
+const $bookmarkBtn = document.querySelector(".bookmark-btn");
 
-$navBtn.addEventListener("click", () => {
+initEvents();
+
+function initEvents() {
+    $navBtn.addEventListener('click', handleMenu);
+    $backBtn.addEventListener('click', showRewardModal);
+    $bookmarkBtn.addEventListener('click', handleBookmark);
+}
+
+function handleMenu() {
     const $navMenu = document.querySelector(".nav-menu");
     const $navImg = $navBtn.querySelector("img");
     const displayStatus = window.getComputedStyle($navMenu).display;
@@ -12,4 +22,23 @@ $navBtn.addEventListener("click", () => {
         $navMenu.style.display = "none";
         $navImg.src = "./images/icon-hamburger.svg";
     }
-});
+}
+
+function showRewardModal() {
+    const $rewardModal = document.querySelector(".reward");
+    $rewardModal.style.display = "block";
+
+    const $closeBtn = $rewardModal.querySelector(".closemodal-btn");
+    $closeBtn.addEventListener('click', () => {
+        $rewardModal.style.display = "none";
+    })
+}
+function handleBookmark() {
+    $bookmarkBtn.classList.toggle("bookmarked");
+    const $imgBtn = $bookmarkBtn.querySelector("img");
+    if ($bookmarkBtn.classList.contains("bookmarked")) {
+        $imgBtn.src = "./images/icon-bookmark-true.svg";
+    } else {
+        $imgBtn.src = "./images/icon-bookmark.svg";
+    }
+}
